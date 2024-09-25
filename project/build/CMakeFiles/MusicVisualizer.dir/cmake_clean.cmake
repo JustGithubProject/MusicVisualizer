12ece468320a064ace073b091bcaedfe45cbdd76
JustGithubProject <Kropivadanil@gmail.com>
2024-09-25 18:07:11 +0300

    Basic project structure

---
+++ project/build/CMakeFiles/MusicVisualizer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MusicVisualizer.dir/src/main.cpp.o"
+  "CMakeFiles/MusicVisualizer.dir/src/main.cpp.o.d"
+  "MusicVisualizer"
+  "MusicVisualizer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MusicVisualizer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
